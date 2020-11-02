d1469733d30e3f459aaa94efe668a03cd00389f7
Tristan <tristan15thompson@gmail.com>
2020-11-02 11:07:17 -0800

    deposit and wirthdraw functions done

--- app.py
+++ app.py
@@ -7,8 +7,23 @@ class BankAccount:
 
     def __init__(self, full_name):
         self.full_name = full_name
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f"Amount Deposited: {amount}")
+        return
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+        if self.balance < 0:
+            print("Insufficient funds.")
+            self.balance = self.balance + amount - 10 #adds withdraw back to account and adds a 10 dollar overdraft fee
+            return
+        print(f"Amount Withdrawn: {amount}")
+        return
 
 
 
 my_account = BankAccount("Tristan")
+my_account.deposit(100)
+my_account.withdraw(110)
+
 print(my_account.balance)
