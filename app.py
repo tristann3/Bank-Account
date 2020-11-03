cae15b28944609b42a2f07ea78921c5441df1527
Tristan <tristan15thompson@gmail.com>
2020-11-03 00:46:23 -0800

    prettier output

--- app.py
+++ app.py
@@ -9,31 +9,37 @@ class BankAccount:
         self.full_name = full_name
     def deposit(self, amount):
         self.balance = self.balance + amount
-        print(f"Amount Deposited: {round(amount, 2)}")
+        print(f"\nAmount Deposited: {round(amount, 2)}")
         return
     def withdraw(self, amount):
         self.balance = self.balance - amount
         if self.balance < 0:
-            print("Insufficient funds.")
+            print("\nInsufficient funds. You have been charged an overdraft fee")
             self.balance = self.balance + amount - 10 #adds withdraw back to account and adds a 10 dollar overdraft fee
             return
-        print(f"Amount Withdrawn: {round(amount, 2)}")
+        print(f"\nAmount Withdrawn: {round(amount, 2)}")
         return
     def get_balance(self):
-        print(f"You currently have ${round(self.balance, 2)}")
+        print(f"\nYou currently have ${round(self.balance, 2)}")
         return self.balance
     def add_interest(self):
         interest = self.balance *  0.00083
         self.balance = self.balance + interest
         return
     def print_receipt(self):
-        print(f"{self.full_name}\nAccount No.: {self.account_number} \nRouting No.: {self.routing_number}\nBalance: ${round(self.balance, 2)}")
+        print(f"\n{self.full_name}\nAccount No.: {self.account_number} \nRouting No.: {self.routing_number}\nBalance: ${round(self.balance, 2)}")
 
 
 
-my_account = BankAccount("Tristan")
-my_account.deposit(100)
-my_account.add_interest()
-print(my_account.get_balance())
-my_account.print_receipt()
+tristan_account = BankAccount("Tristan Thompson")
+elon_account = BankAccount("Elon Musk")
+joi_account = BankAccount("Joi Anderson")
+
+tristan_account.deposit(100)
+tristan_account.print_receipt()
+tristan_account.add_interest()
+tristan_account.print_receipt()
+tristan_account.withdraw(1000)
+tristan_account.print_receipt()
+
 
